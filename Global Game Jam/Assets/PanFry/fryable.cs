0504142b59084d4fe9fc9af4cd31d19a432f0842
Oliver Puffer <oliver@pop-os.localdomain>
2021-01-30 15:12:26 -0800

    FRYING MECHANIC

--- Global Game Jam/Assets/PanFry/fryable.cs
+++ Global Game Jam/Assets/PanFry/fryable.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditorInternal;
+#endif
 
 public class fryable : MonoBehaviour
 {
     // Start is called before the first frame update
     private float timeOfFry;
     private float burnTime = 10.5f;
-    private bool burned; 
+    private float doneTime = 5f;
+    private bool burned;
+    private bool isDone;
     private SpriteRenderer spriteR;
     void Start()
     {
@@ -22,11 +27,20 @@ public class fryable : MonoBehaviour
 
         if (timeOfFry > burnTime && !burned) {
             burned = true; 
+            isDone = false;
             this.spriteR.color = Color.black;
+        } else if (timeOfFry > doneTime && timeOfFry < burnTime){
+            isDone = true;
+            this.spriteR.color = new Color(222f, 183f, 0f, 1f);
         }
     }
 
     bool isBurned(){
         return timeOfFry > burnTime;
+    }
+
+    void OnMouseDown(){
+        Debug.Log("Clicked Pan!");
+        DestroyImmediate(gameObject);
     }
 }
