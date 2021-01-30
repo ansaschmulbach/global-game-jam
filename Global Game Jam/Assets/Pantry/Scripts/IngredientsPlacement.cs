3fc1af8eac17ad819d75f0acf64befc8c7a4b551
Viansa Schmulbach <ansa@berkeley.edu>
2021-01-30 15:24:14 -0800

    fixing merge conflicts

--- Global Game Jam/Assets/Pantry/Scripts/IngredientsPlacement.cs
+++ Global Game Jam/Assets/Pantry/Scripts/IngredientsPlacement.cs
@@ -9,20 +9,20 @@ public class IngredientsPlacement : MonoBehaviour
     private Random random = new Random();
 
     // Start is called before the first frame update
-    void Start(int height, int width)
-    {
-        int low = (0 - width)/2;
-        int hight = width / 2;
-
-        int x = 0;
-        foreach (int i in ingredients)
-        {
-            x = random.Next(low, high);
-            GameObject ingredient = Instantiate(i, new Vector3(x, 0, 0), Quaternion.identity);
-            ingredient.transform.SetParent(Background.transform, false);
-        }
-        
-    }
+    // void Start(int height, int width)
+    // {
+    //     int low = (0 - width)/2;
+    //     int hight = width / 2;
+    //
+    //     int x = 0;
+    //     foreach (int i in ingredients)
+    //     {
+    //         x = random.Next(low, high);
+    //         GameObject ingredient = Instantiate(i, new Vector3(x, 0, 0), Quaternion.identity);
+    //         ingredient.transform.SetParent(Background.transform, false);
+    //     }
+    //     
+    // }
 
     // Update is called once per frame
     void Update()
