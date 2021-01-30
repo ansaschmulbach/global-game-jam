2dbc8e1a22abaddc1d14734a8fbf1dd81195675b
emmaguo13 <emmaguo@berkeley.edu>
2021-01-30 14:02:31 -0800

    placements

--- Global Game Jam/Assets/Pantry/Scripts/IngredientsPlacement.cs
+++ Global Game Jam/Assets/Pantry/Scripts/IngredientsPlacement.cs
@@ -1,25 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Text;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class IngredientsPlacement : MonoBehaviour
 {
-    public List<GameObject> ingredients;
+    [SerializeField] public List<GameObject> ingredients;
     public GameObject frame;
-    private Random random = new Random();
+    public GameObject pantryFrame;
+    public System.Random random = new System.Random();
+
 
     // Start is called before the first frame update
     void Start(int height, int width)
     {
+     
+       
         int low = (0 - width)/2;
-        int hight = width / 2;
-
-        int x = 0;
-        foreach (int i in ingredients)
+        int high = width / 2;
+        int rand = random.Next(low, high);
+        foreach (GameObject i in ingredients)
         {
-            x = random.Next(low, high);
-            GameObject ingredient = Instantiate(i, new Vector3(x, 0, 0), Quaternion.identity);
-            ingredient.transform.SetParent(Background.transform, false);
+            rand = random.Next(low, high);
+            GameObject ingredient = Instantiate(i, new Vector3(rand, 0, 0), Quaternion.identity);
+            ingredient.transform.SetParent(pantryFrame.transform, false);
         }
         
     }
@@ -30,3 +35,5 @@ public class IngredientsPlacement : MonoBehaviour
         
     }
 }
+
+
