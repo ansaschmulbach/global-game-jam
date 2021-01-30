bd30a836de724a188c7c6bf0389cdc27889d28d6
Oliver Puffer <oliver@pop-os.localdomain>
2021-01-30 10:11:04 -0800

    Figured out movement

---
+++ Global Game Jam/Assets/kitchen/Scripts/furnitureController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditorInternal;
+#endif
+using UnityEngine;
+
+
+public class furnitureController : MonoBehaviour
+{
+    public ClickMove player;
+
+    public void setPlayerTarget(){
+        Debug.Log("Clicked");
+        Vector3 pos = this.transform.position;
+        player.target = new Vector3(pos.x, pos.y, player.transform.position.z);
+    }
+
+    void OnMouseDown(){
+        setPlayerTarget();
+    }
+
+
+}
