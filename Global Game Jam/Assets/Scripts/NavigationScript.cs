0d71fca2c774a8d3a12e59ad6781b706d78d6ac0
Oliver Puffer <oliver@pop-os.localdomain>
2021-01-30 18:40:02 -0800

    Created return to kitchen button on pantry screen

---
+++ Global Game Jam/Assets/Scripts/NavigationScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameManager manager;
+
+    void Awake()
+    {
+        this.manager = GetComponent<GameManager>();
+    }
+
+    public void GoToKitchen()
+    {
+        
+        GameManager.instance.LoadKitchen();
+    }
+    public void HAYO()
+    {
+        print("HAYO");
+    }
+}
