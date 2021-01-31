76c48c68c7f2dab9f09b4514bf097c9109e6be8b
emmaguo13 <emmaguo@berkeley.edu>
2021-01-30 18:04:44 -0800

    oh yeayea

--- Global Game Jam/Assets/Pantry/Scripts/IngredientsPlacement.cs
+++ Global Game Jam/Assets/Pantry/Scripts/IngredientsPlacement.cs
@@ -12,26 +12,34 @@ public class IngredientsPlacement : MonoBehaviour
     public GameObject tempIngredient;
     public System.Random random = new System.Random();
     private static System.Timers.Timer timer;
+    private int height = 750;
+    private int width = 1210;
+
+    private int low = 100;
+    private int high = 1210 - 100;
+    private List<GameObject> instIngredients;
 
     // Start is called before the first frame update
 
     void Start()
     {
+        /*
         int height = 750;
         int width = 1210;
        
         int low = 100;
         int high = width-100;
-        int rand = random.Next(low, high);
-        
+         */
 
-        //foreach (GameObject i in ingredients)
+        int rand = random.Next(low, high);
+ 
         for(int i = 0; i < 5; i ++)// shelves
         {
             for(int j = 0; j < 10; j ++) {//ingredients
                 rand = random.Next(low, high);
                 GameObject ingredient = Instantiate(tempIngredient, new Vector3(rand, -i*127-75, 0), Quaternion.identity);
                 ingredient.transform.SetParent(pantryFrame.transform, false);
+                instIngredients.Add(ingredient);
             }
             
         }
@@ -40,16 +48,37 @@ public class IngredientsPlacement : MonoBehaviour
                 rand = random.Next(low, high);
                 GameObject ingredient = Instantiate(tempIngredient, new Vector3(rand, -5*127-50, 0), Quaternion.identity);
                 ingredient.transform.SetParent(pantryFrame.transform, false);
+                instIngredients.Add(ingredient);
         }
         
     }
+
 
     // Update is called once per frame
     void Update()
     {
         int rand = random.Next(1, 10000);
+        int randNum = random.Next(low, high);
+        int shelfNum = 1;
+        int count = 0;
         if (rand == 20)
         {
+
+           foreach(GameObject i in instIngredients)
+            {
+                if (count >= 10)
+                {
+                    count = 0;
+                    shelfNum++;
+                }
+                //update position
+                i.transform.position = new Vector3(randNum, -shelfNum * 127 - 75, 0);
+                count++;
+
+            }
+
+
+
 
         }
 
