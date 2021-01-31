0882e973bc450815c7e584f8a1fc63e2e446a99f
Viansa Schmulbach <ansa@berkeley.edu>
2021-01-30 16:18:39 -0800

    renaming conventions

--- Global Game Jam/Assets/Scripts/Choppable.cs
+++ Global Game Jam/Assets/Scripts/Choppable.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chop : Ingredient
+public class Choppable : Ingredient
 {
     
     [SerializeField] private string name;
--- Global Game Jam/Assets/Scripts/Utensil.cs
+++ Global Game Jam/Assets/Scripts/Utensil.cs
@@ -1,8 +1,34 @@
+
+using System;
 using UnityEngine;
 
-public abstract class Utensil : MonoBehaviour
+public class Utensil : MonoBehaviour
 {
+    private GameManager gm;
+    private Furniture parent;
+    private Ingredient ing;
     
-    public abstract void Cook();
+    void Start()
+    {
+        gm = GameManager.instance;
+        parent = GetComponentInParent<Furniture>();
+    }
+    
+    public void Cook()
+    {
+        GameObject inv = gm.gameState.inventory;
+        if (inv.TryGetComponent(out Choppable chop))
+        {
+            chop.Cook();
+            gm.gameState.inventory = null;
+        }
+    }
 
+    private void OnMouseDown()
+    {
+        if (parent.IsSelected())
+        {
+            Cook();
+        }
+    }
 }
