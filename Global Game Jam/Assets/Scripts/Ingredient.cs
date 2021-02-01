d8f2ecf62280a7a42567e2a96608369115afc777
emmaguo13 <emmaguo@berkeley.edu>
2021-01-31 16:15:11 -0800

    merge

--- Global Game Jam/Assets/Scripts/Ingredient.cs
+++ Global Game Jam/Assets/Scripts/Ingredient.cs
@@ -55,17 +55,14 @@ public abstract class Ingredient : MonoBehaviour
 
     public abstract string GetRecipeLine();
     protected abstract void CookIngredient();
-<<<<<<< HEAD
-=======
 
->>>>>>> 4d276e8af3066fb545db2d70f24609b524c74232
 
     private void OnMouseUp()
     {
         Console.WriteLine("hi");
         if (Time.time - downClickTime <= ClickDeltaTime)
         {
-<<<<<<< HEAD
+
             if (GameManager.instance.gameState.inventory == null)
             {
                 Inventory();
@@ -93,13 +90,6 @@ public abstract class Ingredient : MonoBehaviour
     }
      */
 
-
-=======
-            Inventory();
-        }
-    }
-    
->>>>>>> 4d276e8af3066fb545db2d70f24609b524c74232
 
     protected abstract Commands getCommand();
 
